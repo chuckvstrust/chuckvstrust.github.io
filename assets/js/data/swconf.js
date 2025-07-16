359c3e808b0f4fa8632eb743f9f6bb6a2df76d01
chuckvstrust <chuck@chuckvstrust.com>
2025-07-15 23:14:29 -0400

    Deploy final clean build

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752628528',resources: [
+    cacheName: 'chirpy-1752635653',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
