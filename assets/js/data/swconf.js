7dfa03fe0368527ad32142253ed88bc63b2f7e3c
chuckvstrust <chuck@chuckvstrust.com>
2025-07-16 11:36:51 -0400

    Deploy About page update

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752678344',resources: [
+    cacheName: 'chirpy-1752680192',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
